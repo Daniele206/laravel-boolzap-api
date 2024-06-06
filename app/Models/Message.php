5b6646ab78fe83a447da7869f902bfbdd7cbd339
Daniele206 <danieledeieso2004@gmail.com>
2024-06-06 16:18:03 +0200

    [add] model->relation

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    public function contact(){
+        return $this->belongsTo(Contact::class);
+    }
+
+    public function chat(){
+        return $this->belongsTo(Chat::class);
+    }
 }
